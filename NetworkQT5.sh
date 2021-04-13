ea2c11dfcd434779372d65b6b903bf191c8c9d2b
Samuel Martinez <junior.mar248@gmail.com>
2021-04-12 21:13:33 -0400

    Update NetworkQT5.sh

--- NetworkQT5.sh
+++ NetworkQT5.sh
@@ -18,10 +18,31 @@ then
 elif [ $num=="2" ] 
 then
      echo Enter the host address you would like to Allow 
-echo "Hostname:" 
-read host 
-sudo iptables -A INPUT -s shost -j ACCEPT 
-sudo iptables -L 
-
-     [ $num=="3" ] echo Enter the mac address you would like to Block echo "Mac Address: read mac sudo iptables -A INPUT -m mac --mac-source Smac - REJECT sudo iptables -L ADR ADR Block Telnet Block Ping elif [ then elif [ then elif [ Snue="4"]
-elif t snus"4"] then echo Enter the mac address you would like to Allow echo "Mac Address: read mac sudo iptables sudo iptables -L -A INPUT -m mac --mac-source $mac -j ACCEPT elif ( snun="5" ] then sudo iptables -A INPUT -p tcp --dport 23 - REJECT echo Telnet port 23 is blocked elif [ Snun="6" ] then else sudo iptables -A INPUT -p tcp --dport 7 - REJECT fi echo Not a valid entry
+     echo "Hostname:" 
+     read host 
+     sudo iptables -A INPUT -s shost -j ACCEPT 
+     sudo iptables -L 
+elif [ $num=="3" ] 
+then
+     echo Enter the mac address you would like to Block 
+     echo "Mac Address: 
+     read mac 
+     sudo iptables -A INPUT -m mac --mac-source $mac -j REJECT 
+     sudo iptables -L 
+elif [ $num=-"4" ] 
+then 
+     echo Enter the mac address you would like to Allow 
+     echo "Mac Address: 
+     read mac 
+     sudo iptables -A INPUT -m mac --mac-source $mac -j ACCEPT 
+     sudo iptables -L 
+elif [ $num=="5" ] 
+then 
+     sudo iptables -A INPUT -p tcp --dport 23 - REJECT 
+     echo Telnet port 23 is blocked 
+elif [ Snun="6" ] 
+then 
+     sudo iptables -A INPUT -p tcp --dport 7 - REJECT 
+else     
+     echo Not a valid entry
+fi
